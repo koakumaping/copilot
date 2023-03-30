41ff7ebb25fb84e4988415233e3fc4039033bc1e
平奕峰 <koakumaping@163.com>
2023-03-30 08:58:38 +0800

    del: useless code

--- message.lua
+++ message.lua
@@ -10,8 +10,6 @@ local topRightMask = lcd.loadMask('./bitmaps/tr.png')
 local bottomLeftMask = lcd.loadMask('./bitmaps/bl.png')
 local bottomRightMask = lcd.loadMask('./bitmaps/br.png')
 
-local messageMask = lcd.loadMask('./bitmaps/message.png')
-
 message = ''
 messageStartTime = 0
 messageEndTime = 0
@@ -27,11 +25,6 @@ function draw(widget)
   lcd.color(lcd.RGB(255, 255, 255, 0.6))
   lcd.drawFilledRectangle(moduleX, moduleY, moduleWidth, moduleHeight)
 
-  -- lcd.color(lcd.RGB(255, 255, 255, 0))
-  -- lcd.drawMask(moduleX, moduleY, topLeftMask)
-  -- lcd.drawMask(moduleX + moduleWidth - 6, moduleY, topRightMask)
-  -- lcd.drawMask(moduleX, moduleY + moduleHeight - 6, bottomLeftMask)
-  -- lcd.drawMask(moduleX + moduleWidth - 6, moduleY + moduleHeight - 6, bottomRightMask)
   lcd.color(widget.libs.var.textColor)
   lcd.drawText(moduleX + 8, moduleY + 8, message)
   -- if f then f(widget, x + 8, y + 8) end
@@ -54,14 +47,8 @@ function module.paint(widget, x, y)
     if messageEndTime <= os.clock() then
       message = ''
       lcd.setClipping(moduleX, moduleY, moduleWidth, moduleHeight)
-      -- print('message unpainted', os.clock())
     end
-    -- lcd.color(widget.libs.var.textColor)
-    -- lcd.drawMask(moduleX, moduleY, messageMask)
-    -- lcd.color(widget.libs.var.whiteColor)
-    -- lcd.drawText(moduleX + 16, moduleY + 16, message)
     draw(widget)
-    -- print('message painted', os.clock())
   end
 end
 
